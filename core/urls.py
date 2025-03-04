47ca78a29c616e1793243bdefb9d4716b076ce2e
Fotsingboris <fotsingtchoupe1@gmail.com>
2025-03-04 16:44:23 +0100

    added sidebar

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('user-management/', user_management, name='user_management'),
     path('dashboard/', dashboard, name='dashboard'),  # Create a simple dashboard view if needed
-        path('login/', login_view, name='login'),  # URL for the login page
+        path('', login_view, name='login'),  # URL for the login page
         path('logout/', user_logout, name='logout'),
 
 ]
