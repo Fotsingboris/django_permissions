a60f37cd26684b2484e9688d9624b925463fb552
Fotsingboris <fotsingtchoupe1@gmail.com>
2025-03-04 02:08:16 +0100

    fix:trasn in teh settingds

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
+from core.models import *
+
 class CustomUserAdmin(UserAdmin):
     filter_horizontal = ('user_permissions',)
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)  # Display name in the admin list
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+    list_filter = ('category',)
+    search_fields = ('title', 'content')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
